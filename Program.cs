7a1b6c84a8be78fa3eff85cf085a71a1b7d9b2b2
arshak <arshakr10@gmail.com>
2019-09-08 16:21:27 +0300

    nor

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Arshak";
+            string last = "Lazaryan";
+            int age = 24;
+            Console.WriteLine($"my name is: {name }");
+            Console.WriteLine($"my last name is:{ last}");
+            Console.WriteLine($"my age: {age}");
+        }
+    }
+}
